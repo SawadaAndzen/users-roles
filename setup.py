ac685b05dbdb533cda269e223782569cb74ba799
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2024-09-01 13:51:15 +0300

    done

---
+++ setup.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import django
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
